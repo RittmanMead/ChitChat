d07ddb8b37d9fd7551d22d8293ad346ee4f7ba0a
barretbse <federico.venturin@rittmanmead.com>
2018-06-19 08:49:36 +0200

    Update deploySocialize.py

--- ChitChatInstaller/deploySocialize.py
+++ ChitChatInstaller/deploySocialize.py
@@ -1,7 +1,7 @@
 connect('weblogic', 'Admin123', 't3://localhost:9500')
 edit()
 startEdit()
-deploy('Socialize', '/app/oracle/ChitChatInstaller/war/Socialize.war', 'targets=bi_server1')
+deploy('Socialize', '/app/oracle/ChitChatInstaller/war/Socialize.war', targets='bi_server1')
 save()
 activate()
 exit()
